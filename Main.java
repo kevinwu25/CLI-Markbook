a55ae2f2f317e90b6decaa8b957eedfaf3463e79
MingzeJin <36276890+MingzeJin@users.noreply.github.com>
2018-02-26 12:48:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,37 +7,38 @@ public class Main {
 	    Scanner input = new Scanner(System.in);
             // All arrays
 	    String names[];
-        int studentNum[];
+            int studentNum[];
 
-        System.out.print("What's your name? ");
-        String teacher = input.nextLine();
+            System.out.print("What's your name? ");
+            String teacher = input.nextLine();
 
-        System.out.print("Hi " + teacher + ", how many students do you have? ");
-        int classSize = Integer.parseInt(input.nextLine());
-        names = new String[classSize];
-        studentNum = new int[classSize];
-
-        System.out.println("What are their names?");
-        for (int i = 0; i < classSize; i++) {
-            names[i] = input.nextLine();
-        }
+            System.out.print("Hi " + teacher + ", how many students do you have? ");
+            int classSize = Integer.parseInt(input.nextLine());
+            names = new String[classSize];
+            studentNum = new int[classSize];
 	    
-	System.out.print("What year will your class be graduating?");
-        int gradYear = input.nextInt();
-
-        Arrays.sort(names);
-
-        System.out.println("Here's the class list, what are their student numbers? (please input according to alphabetical order)");
-        for (int i = 0; i < classSize; i++) {
-            if (i < 10) {
-                System.out.print("0" + (i + 1) + ": " + names[i] + " ");
-                studentNum[i] = input.nextInt();
-            } else {
-                System.out.println((i + 1) + ": " + names[i] + " ");
-                studentNum[i] = input.nextInt();
+	    System.out.print("What year will your class graduate? ");
+            int gradYear = input.nextInt();
+   
+            System.out.println("What are their names?");
+            for (int i = 0; i < classSize; i++) {
+                names[i] = input.nextLine();
+            }
+	    
+	    System.out.print("What year will your class be graduating?");
+            int gradYear = input.nextInt();
+    
+            Arrays.sort(names);
+  
+            System.out.println("Here's the class list, what are their student numbers? (please input according to alphabetical order)");
+            for (int i = 0; i < classSize; i++) {
+                if (i < 10) {
+                    System.out.print("0" + (i + 1) + ": " + names[i] + " ");
+                    studentNum[i] = input.nextInt();
+                } else {
+                    System.out.println((i + 1) + ": " + names[i] + " ");
+                    studentNum[i] = input.nextInt();
             }
         }
-
-       System.out.println("");
     }
 }
